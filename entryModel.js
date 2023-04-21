ee78751d8d33f2141a91b8a4b13ac74dc2af391a
Roody23 <rudrapartapsingh43@gmail.com>
2023-04-21 13:57:57 +0530

    created models 1

--- entryModel.js
+++ entryModel.js
@@ -3,12 +3,11 @@ const sql = require("../config/db.config.js");
 const Entry = function (entry) {
   this.UserID = entry.UserID;
   this.Username = entry.Username;
-  this.Task_description = task.Task_description;
   this.Create_date = new Date();
 };
 
-Task.getTasks = (userID, result) => {
-  sql.query(`SELECT Task_title, Create_date FROM Tasks WHERE UserID = ?`, [userID], (err, res) => {
+Task.getEnteries = (userID, result) => {
+  sql.query(`SELECT * FROM Entries WHERE userID = ?`, [userID], (err, res) => {
     if (err) {
       console.log("error: ", err);
       result(err, null);
@@ -18,17 +17,3 @@ Task.getTasks = (userID, result) => {
     result(null, res);
   });
 };
-
-Task.getTask = (userID, taskID, result) => {
-  sql.query(`SELECT * FROM Tasks WHERE UserID = ? AND ID = ?`,[userID,taskID], (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-
-    if (res.length) {
-      console.log("found task: ", res[0]);
-      result(null, res[0]);
-      return;
-    }
